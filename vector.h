3f405d66ebb9718537a60a7e705d3b5c59d35c74
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-11-12 15:19:16 -0700

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,77 +1,187 @@
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
 
-#include <memory> // unique pointer
+#include <memory>    // unique pointer
+#include <algorithm> // min
 
 template <typename T>
-class Vector
+class vector
 {
-	std::size_t count;         // Number of actually stored objects.
-	std::size_t capacity;      // Allocated capacity.
-	std::unique_ptr<T[]> data; // Data element.
+	std::size_t count;             // Number of actually stored objects.
+	std::size_t reservedSize;      // Allocated reservedSize.
+	std::unique_ptr<T[]> elements; // Data elements.
 
 public:
-	Vector() : count(0), capacity(0), data(nullptr) { };
-	
+	using iterator = T * ;
+	using const_iterator = const T*;
+	using reverse_iterator = std::reverse_iterator<iterator>;
+	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+	typename vector<T>::iterator begin() noexcept { return elements.get(); }
+	typename vector<T>::iterator end() noexcept { return iterator(elements.get() + count); }
+	typename vector<T>::const_iterator cbegin() const noexcept { return begin(); }
+	typename vector<T>::const_iterator cend() const noexcept { return end(); }
+	typename vector<T>::reverse_iterator rbegin() noexcept { return reverse_iterator(elements.get() + count); }
+	typename vector<T>::reverse_iterator rend() noexcept { return reverse_iterator(elements.get()); }
+	typename vector<T>::const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(elements.get() + count); }
+	typename vector<T>::const_reverse_iterator crend() const noexcept { return const_reverse_iterator(elements.get()); }
+
+	vector() : count(0), reservedSize(0), elements(nullptr) { }
+
 	// Copy ctor.
-	Vector(Vector const &rhs) : count(rhs.count), capacity(rhs.capacity)
+	vector(vector const &rhs) : count(rhs.count), reservedSize(rhs.reservedSize)
 	{
-		std::unique_ptr<T[]> data = std::make_unique<T[]>(capacity);
+		std::unique_ptr<T[]> elements = std::make_unique<T[]>(reservedSize);
 
 		for (std::size_t i = 0; i < count; i++)
-			data[i] = rhs.data[i];
-	};
-	
-	void clear() { data.release(); };
+			elements[i] = rhs.elements[i];
+	}
 
-	// Provides memory management.
-	Vector &operator= (Vector const &rhs)
+	// Move ctor.
+	vector(vector&& rhs) noexcept { swap(rhs); }
+
+	// Assignment ctor.
+	vector<T>& operator= (const vector& rhs)
 	{
-		std::unique_ptr<T[]> temp(static_cast<T*>(data.release()));
-
-		count = rhs.count;
-		capacity = rhs.capacity;
-		data.reset(new T[capacity]);
-		
-		for (std::size_t i = 0; i < count; i++)
-			data[i] = temp[i];
-
+		swap(vector(rhs));
 		return *this;
-	};
+	}
 
-	// Adds new value, and if needed allocates more space.
+	// Move assignment ctor.
+	vector<T>& operator= (vector<T>&& rhs)
+	{
+		swap(rhs);
+		return *this;
+	}
+
+	~vector() { clear(); }
+
+	void clear() 
+	{ 
+		while (0 < count)
+			elements[--count].~T();
+		reservedSize = 0;
+	}
+
 	void push_back(T const &d)
 	{
-		if (capacity == count)
+		if (reservedSize == count)
 			resize();
-		data[count++] = d;
-	};
+		elements[count++] = d;
+	}
 
 	void pop_back()
 	{
 		if (count == 0)
 			return;
 		count--;
-	};
+	}
 
-	size_t size() const { return count; };
+	size_t size() const { return count; }
+	bool empty() const { return count == 0; }
 
-	// Bracketed set/get.
-	T const &operator[] (size_t i) const { return data[i]; };
-	T& operator[] (size_t i) { return data[i]; };
+	T const &operator[] (size_t i) const { return elements[i]; }
+	T& operator[] (size_t i) { return elements[i]; }
+
+	T const & at(size_t i) const { return elements[i]; }
+	T& at(size_t i) { return elements[i]; }
+
+	T* data() { return elements.get(); }
+	const T* data() const { return elements; }
+
+	T& front() const { return elements[0]; }
+	T& back() const { return elements[count - 1]; }
+
+	void insert(size_t i, T& d)
+	{
+		if (i < 0 || i > count - 1)
+			return;
+
+		if (reservedSize == count)
+			resize();
+
+		std::copy(&elements[i], (elements.get() + count), &elements[i + 1]);
+		elements[i] = d;
+		count++;
+	}
+
+	void insert(iterator it, const T& d)
+	{
+		size_t i = it - begin();
+
+		if (i < 0 || i > count - 1)
+			return;
+
+		if (reservedSize == count)
+			resize();
+
+		std::copy(&elements[i], (elements.get() + count), &elements[i + 1]);
+		elements[i] = d;
+		count++;
+	}
+
+	void erase(size_t pos)
+	{
+		if (pos < 0 || pos >= count)
+			return;
+
+		--count;
+		std::copy(&elements[pos + 1], (elements.get() + count), &elements[pos]);
+	}
+
+	void erase(iterator it)
+	{
+		size_t pos = it - begin();
+
+		if (pos < 0 || pos >= count)
+			return;
+
+		--count;
+		std::copy(&elements[pos + 1], (elements.get() + count), &elements[pos]);
+	}
+
+	void assign(size_t n, const T& d)
+	{
+		for (size_t i = 0; i < n; i++)
+			push_back(d);
+	}
+
+	void swap(vector<T>& rhs)
+	{
+		using std::swap;
+
+		swap(count, rhs.count);
+		swap(reservedSize, rhs.reservedSize);
+		swap(elements, rhs.elements);
+	}
+
+	size_t max_size() const noexcept { return std::numeric_limits<size_t>::max(); }
+	size_t capacity() const noexcept { return reservedSize; }
+	void reserve(size_t n)
+	{
+		std::unique_ptr<T[]> newElements(static_cast<T*>(elements.release()));
+		elements.reset(new T[reservedSize]);
+
+		count = std::min(n, count);
+		std::copy(elements.get(), &elements[count], newElements.get());
+		reservedSize = n;
+		elements = std::move(newElements);
+
+	}
+	void resize(size_t n) { reserve(n); }
+	void shrink_to_fit() { reserve(count); }
 
 private:
-	// Allocates double old space.
+	// Allocates double the old space.
 	void resize()
 	{
-		capacity = capacity ? capacity*2 : 1;
+		reservedSize = reservedSize ? reservedSize * 2 : 1;
 
-		std::unique_ptr<T[]> spTemp(static_cast<T*>(data.release()));
-		data.reset(new T[capacity]);
+		std::unique_ptr<T[]> spTemp(static_cast<T*>(elements.release()));
+		elements.reset(new T[reservedSize]);
 
-		for (std::size_t i = 0; i < count; i++)
-			data[i] = spTemp[i];
-	};
+		std::copy(spTemp.get(), &spTemp[count], elements.get());
+	}
 };
 
 #endif
