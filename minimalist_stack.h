edaeb808ad74f7773f4baec26492deaadf69cad2
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-12-21 08:39:18 -0700

    Update minimalist_stack.h

--- minimalist_stack.h
+++ minimalist_stack.h
@@ -7,18 +7,14 @@ class stack : public std::vector<T>
 {
 private:
 	std::vector<T> data;
-	T& operator[] (int i) { return data[i]; }
 public:
 	std::size_t size() { return data.size(); }
 	std::size_t empty() { return data.empty(); }
-	void push(T item) { data.push_back(item); }
-	T pop() { T& d = data.back(); data.pop_back(); return d; }
-	friend std::ostream& operator<< (std::ostream& os, stack<T>& s)
-	{
-		std::size_t n = s.size();
-		while (n)
-			os << s[--n] << " ";
+	void push(const T& item) { data.push_back(item); }
+	T& pop() { T& d = data.back(); data.pop_back(); return d; }
+	friend std::ostream& operator<< (std::ostream& os, stack<T>& s) {
+		std::size_t n = s.data.size();
+		while (n) os << s.data.at(--n) << " ";
 		return os;
 	}
 };
-
