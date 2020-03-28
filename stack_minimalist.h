5566efd53825cbdc0238392fe47f6602e5620145
Jim Eli <31859133+JimEli@users.noreply.github.com>
2020-03-28 07:36:02 -0700

    Create stack_minimalist.h

---
+++ stack_minimalist.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+template <class T>
+class stack : public std::vector<T>
+{
+private:
+	std::vector<T> data;
+
+public:
+	std::size_t size() { return data.size(); }
+
+  std::size_t empty() { return data.empty(); }
+	
+  T& operator[] (int i) { return data[i]; }
+	
+  void push(T item) { data.push_back(item); }
+	
+  T pop() { T& d = data.back(); data.pop_back(); return d; }
+	
+  friend std::ostream& operator<< (std::ostream& os, stack<T>& s)
+	{
+		std::size_t n = s.size();
+    
+		while (n)
+			os << s[--n];
+		return os;
+	}
+};
+
